cc497e3bd5c7ab2e46e8d5fd369928a81a0490c2
lynnjepsen <lynnjepsen@google.com>
2021-01-08 20:36:08 -0800

    lay out cover and cursor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import {createUseStyles} from 'react-jss';
 
-
 const useStyles = createUseStyles({
+  root: {
+    position: 'relative',
+    marginLeft: 16
+  },
   paragraph: {
     fontFamily: 'Roboto Mono',
     lineHeight: '24px',
     width: 'calc(100% - 32px)',
-    marginLeft: 16,
     '&:before': {
       display: 'inline-block',
       content: '""',
@@ -23,18 +25,41 @@ const useStyles = createUseStyles({
       verticalAlign: -8
     }
   },
+  bottomCover: {
+    backgroundColor: '#FFF',
+    width: 'calc(100% - 32px)',
+    height: 24,
+    position: 'absolute',
+    top: 40
+  },
+  rightCover: {
+    backgroundColor: '#FFF',
+    width: 'calc(100% - 42px)',
+    height: 48,
+    position: 'absolute',
+    top: 16,
+    left: 10
+  },
+  cursor: {
+    backgroundColor: '#C4C4C4',
+    width: 10,
+    height: 18,
+    position: 'absolute',
+    top: 19,
+    left: 10
+  }
 });
 
 function App() {
   const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       <div className={classes.paragraph}>
         Hi, I’m an A.I. trained to evaluate musical taste. To get started, I’ll need to see your Spotify.
       </div>
-      <div className={classes.paragraph}>
-        I’m just gonna look at what you listen to. I won’t post or change anything
-      </div>
+      <div className={classes.bottomCover}/>
+      <div className={classes.rightCover}/>
+      <div className={classes.cursor}/>
     </div>
   );
 }
