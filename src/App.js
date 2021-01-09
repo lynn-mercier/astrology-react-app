a654a21f8d808ef3191656ee783186fc1ea9f4a1
lynnjepsen <lynnjepsen@google.com>
2021-01-08 20:26:19 -0800

    Layout out scrolling text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {createUseStyles} from 'react-jss';
+
+
+const useStyles = createUseStyles({
+  paragraph: {
+    fontFamily: 'Roboto Mono',
+    lineHeight: '24px',
+    width: 'calc(100% - 32px)',
+    marginLeft: 16,
+    '&:before': {
+      display: 'inline-block',
+      content: '""',
+      width: 0,
+      height: 32,
+      verticalAlign: 0
+    },
+    '&:after': {
+      display: 'inline-block',
+      content: '""',
+      width: 0,
+      height: 8,
+      verticalAlign: -8
+    }
+  },
+});
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={classes.paragraph}>
+        Hi, I’m an A.I. trained to evaluate musical taste. To get started, I’ll need to see your Spotify.
+      </div>
+      <div className={classes.paragraph}>
+        I’m just gonna look at what you listen to. I won’t post or change anything
+      </div>
     </div>
   );
 }
